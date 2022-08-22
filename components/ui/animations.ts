65ff91d7ae9049ca80e7826b3f3c0c4a4b13891b
Stephen O. Arogundade <stephenseun09@gmail.com>
2022-08-22 11:35:36 +0100

    changed animation

--- components/ui/animations.ts
+++ components/ui/animations.ts
@@ -5,8 +5,8 @@ const fadeIn = {
   visible: (i: number) => ({
     opacity: 1,
     transition: {
-      delay: i * 0.2,
-      duration: 1,
+      delay: i * 0.1,
+      duration: 0.5,
       ease: [0.48, 0.15, 0.25, 0.96],
     },
   }),
@@ -21,7 +21,7 @@ const fadeInBottom = {
     opacity: 1,
     y: "0px",
     transition: {
-      delay: i * 0.2,
+      delay: i * 0.1,
       duration: 0.5,
       ease: [0.48, 0.15, 0.25, 0.96],
     },
@@ -37,7 +37,7 @@ const slideInLeft = {
     x: "0%",
     transition: {
       duration: 0.5,
-      delay: 0.2,
+      // delay: 0.1,
       ease: [0.645, 0.045, 0.01, 0.6],
     },
   },
@@ -47,28 +47,27 @@ const slideInRight = {
     opacity: 0,
     x: "100%",
   },
-  visible: (i=0)=>({
+  visible: (i = 0) => ({
     opacity: 1,
     x: "0%",
     transition: {
       duration: 1,
-      delay: i * 0.2,
+      // delay: i * 0.1,
       ease: [0.645, 0.045, 0.01, 0.6],
     },
   }),
 };
 
-
 const slideOutRight = {
   hidden: {
     x: "0%",
   },
-  visible: (i=0)=>({
+  visible: (i = 0) => ({
     opacity: 1,
     x: "100%",
     transition: {
-      duration: 2,
-      delay: i * 0.2,
+      duration: 1.5,
+      delay: i * 0.1,
       ease: [0.645, 0.045, 0.01, 1],
     },
   }),
